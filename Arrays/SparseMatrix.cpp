513b466f2bc5f6f39f3808692a0aa3a6a17b30db
Aman Tiwari <amantiwari8861@gmail.com>
2024-02-27 17:16:39 +0530

    sparse matrix

--- Arrays/SparseMatrix.cpp
+++ Arrays/SparseMatrix.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class SparseMatrix
 {
     int row,col,count;
-    int **matrix;
+    int **matrix,**sparse;
     public:
     SparseMatrix()
     {
@@ -30,6 +30,42 @@ class SparseMatrix
             }            
         }
     }
+    void generateSparse()
+    {
+        int r=0;
+        sparse=new int*[count+1];
+        for (int i = 0; i < row; i++)
+        {
+            sparse[i]=new int[3];
+            if (r==0)
+            {
+                sparse[r][0]=row;
+                sparse[r][1]=col;
+                sparse[r][2]=count;
+            }
+            for (int j = 0; j < col; j++)
+            {
+                if (matrix[i][j]!=0)
+                {
+                    cout<<"hii";
+                    sparse[r][0]=i;
+                    sparse[r][1]=j;
+                    sparse[r][2]=matrix[i][j];
+                    r++;
+                }                
+            }
+        }
+        cout<<"the sparse matrix is \n";
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                cout<<"  "<<sparse[i][j];
+            }
+            cout<<endl;
+        }
+        
+    }
     void printMatrix()
     {
         cout<<"the matrix is :\n";
@@ -46,6 +82,7 @@ class SparseMatrix
 int main()
 {
     SparseMatrix sm;
-    sm.printMatrix();
+    // sm.printMatrix();
+    sm.generateSparse();
     return 0;
 }
